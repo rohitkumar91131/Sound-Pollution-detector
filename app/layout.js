45421d6a8842e4b2a5ef7da381169809122bd750
Rohit kumar <164847892+rohitkumar91131@users.noreply.github.com>
2025-08-20 01:44:52 +0530

    Changes app name to sound pollution meter

--- app/layout.js
+++ app/layout.js
@@ -2,32 +2,29 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+variable: "--font-geist-sans",
+subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+variable: "--font-geist-mono",
+subsets: ["latin"],
 });
 
 export const metadata = {
-  title: "Sound Pollution Checker",
-  description: "Check sound pollution levels in real-time.",
-  verification: {
-    google: "google117d5508caccfad5",
-  },
+title: "Sound Pollution Meter",
+description: "A web app to check how much sound pollution is around yours",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head />
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
-                      }
+export default function RootLayout({ children }) {
+return (
+<html lang="en">
+<body
+className={${geistSans.variable} ${geistMono.variable} antialiased}
+>
+{children}
+</body>
+</html>
+);
+}
+
